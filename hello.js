771c3459b803a60430d784006215773624e14086
Valeriu Bahov <60433187+valeriubahov@users.noreply.github.com>
2021-09-10 10:50:19 -0300

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,2 +1,12 @@
 
-console.log("Hello Valeriu");
+let rls = require("readline-sync");
+let firstNum = rls.question("Insert the first number: ");
+
+let secondNum = rls.question("Insert the second number: ");
+
+let sum = parseInt(firstNum) + parseInt(secondNum);
+
+let msg = "I am " + sum  + " years old";
+console.log(msg);
+
+
