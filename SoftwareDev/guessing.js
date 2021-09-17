172480419285a1198d04a17470dbdf473e657612
Bahov <bahovvaleriu@gmail.com>
2021-09-17 10:14:32 -0300

    2021-09-17

--- SoftwareDev/guessing.js
+++ SoftwareDev/guessing.js
@@ -3,31 +3,28 @@ const rls = require("readline-sync");
 console.log("Try to guess the hidden number inserting a number between 1 and 10. You will have 5 guesses.");
 const GUESSES = 5;
 let tentative = 0;
-const randomNum = Math.ceil(Math.random() * 10);
-while (GUESSES > tentative) {
-    const input = parseInt(rls.question("Please enter a number between 1 and 10 "));
+let input;
 
-    //JS likes to have the Number verificator with a NOT statement
-    if (!Number.isInteger(input)) {
-        console.log("Please enter a number, other characters are not available");
+//NUMBER TO GUESS
+const randomNum = Math.ceil(Math.random() * 10);
+//GAME STARTS
+while (GUESSES > tentative) {
+    do {
+        input = rls.question("Please enter a number between 1 and 10 ");
+    } //JS likes to have the Number verificator with a NOT statement
+    while (isNaN(input) || (input < 1 || input > 10));
+
+    if (input > randomNum) {
+        console.log("WRONG! Your guess is to high");
+        tentative++;
+    }
+    else if (input < randomNum) {
+        console.log("WRONG! Your guess is to low");
+        tentative++;
     }
     else {
-        if (input < 1 || input > 10) {
-            console.log("Please entwer a number between 1 and 10");
-        }
-        else if (input > randomNum) {
-            console.log("WRONG! Your guess is to high");
-            tentative++;
-        }
-        else if (input < randomNum) {
-            console.log("WRONG! Your guess is to low");
-            tentative++;
-        }
-        else {
-            console.log("CORRECT!, the number was " + randomNum);
-            tentative++;
-            break;
-        }
+        console.log("CORRECT!, the number was " + randomNum);
+        tentative++;
+        break;
     }
 }
-
