82d7df926b0f6cc9bf60b90ae8df818eddc7dacc
Bahov <bahovvaleriu@gmail.com>
2021-10-22 12:27:39 -0300

    portolio version 2 almost done

--- HTML&CSS/ASSIGNMENT_3/scripts/scripts.js
+++ HTML&CSS/ASSIGNMENT_3/scripts/scripts.js
@@ -3,24 +3,73 @@ window.onwheel = e => {
         // Scrolling Down with mouse
         if (document.getElementById('check-2').checked == true) {
             var elem = document.getElementById('check-3');
-            elem.checked = true;
+            setTimeout(function () { elem.checked = true; }, 300);
+
         }
         else {
             if (document.getElementById('check-3').checked == false) {
                 var elem = document.getElementById('check-2');
-                elem.checked = true;
+                setTimeout(function () { elem.checked = true; }, 300);
             }
         }
     } else {
         // Scrolling Up with mouse
         if (document.getElementById('check-3').checked == true) {
             var elem = document.getElementById('check-2');
-            elem.checked = true;
+            setTimeout(function () { elem.checked = true; }, 300);
         }
         else {
             var elem = document.getElementById('check-1');
-            elem.checked = true;
+            setTimeout(function () { elem.checked = true; }, 300);
         }
+    }
+};
+
+
+// Check if the user press key up or key down
+document.onkeydown = checkKey;
+function checkKey(e) {
+
+    e = e || window.event;
+
+    if (e.keyCode == '38') {
+        arrowUpScroll();
+    }
+    else if (e.keyCode == '40') {
+        arrowDownScroll();
+    }
+    else if (e.keyCode == '37') {
+        // left arrow
+    }
+    else if (e.keyCode == '39') {
+        // right arrow
+    }
+}
+
+function arrowDownScroll() {
+
+    // Scrolling Down with key down press
+    if (document.getElementById('check-2').checked == true) {
+        var elem = document.getElementById('check-3');
+        setTimeout(function () { elem.checked = true; }, 300);
+    }
+    else {
+        if (document.getElementById('check-3').checked == false) {
+            var elem = document.getElementById('check-2');
+            setTimeout(function () { elem.checked = true; }, 300);
+        }
+    }
+};
+
+function arrowUpScroll() {
+    // Scrolling UP with key UP press
+    if (document.getElementById('check-3').checked == true) {
+        var elem = document.getElementById('check-2');
+        setTimeout(function () { elem.checked = true; }, 300);
+    }
+    else {
+        var elem = document.getElementById('check-1');
+        setTimeout(function () { elem.checked = true; }, 300);
     }
 };
 
@@ -32,10 +81,12 @@ $(function () {
     }
 });
 
-$(document).ready(function() { 
+$(document).ready(function () {
     $('#middleDiv').load('aboutMe.html');
 });
 
-$(document).ready(function() { 
+$(document).ready(function () {
     $('#bottomDiv').load('skills.html');
 });
+
+
