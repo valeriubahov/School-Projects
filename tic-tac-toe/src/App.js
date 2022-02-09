d5207cc7a5617312b372d4e9d280f7d1e7625e81
Valeriu Bahov <60433187+valeriubahov@users.noreply.github.com>
2022-02-09 12:18:59 -0400

    Add files via upload

---
+++ tic-tac-toe/src/App.js
@@ -0,0 +1,14 @@
+import Game from './components/Game/game'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Game />
+      </header>
+    </div>
+  );
+}
+
+export default App;
